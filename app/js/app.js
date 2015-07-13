82bf392e2091ec746e2195df335de574d9003571
nguyenmanh1507 <nguyenmanh1507@gmail.com>
2015-07-13 14:59:47 +0700

    add datetimepicker + config

--- app/js/app.js
+++ app/js/app.js
@@ -13,7 +13,31 @@ var app = (function(document, $) {
 			// Custom JS place here
 			
 			// Datepicker
-			$('#date').datepicker({
+			var now = new Date();
+			var month = now.getMonth();
+			var year = now.getFullYear();
+			var date = now.getDate();
+			var maxMonth = month + 3;
+			var allowTimes = [
+					'10:00', '11:00', '12:00', '13:00', '14:00', 
+					'15:00', '20:00', '21:00', '22:00'
+				];
+
+			if(maxMonth > 12) {
+				year += 1;
+			} 
+
+			if(date > 28) {
+				date -= 3;
+			}
+
+			var maxDate = year + '/' + maxMonth + '/' + date;
+
+			$('#date').datetimepicker({
+				minDate: 0,
+				maxDate: maxDate,
+				minTime: 0,
+				allowTimes: allowTimes
 			});
 
 		};
